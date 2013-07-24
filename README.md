ad0b9a8482abd411e0ece029d5eca6fa221dd245
Karolis Narkevicius <karolis.n@gmail.com>
2013-07-24 15:40:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# grunt-bower-clean
+
+> Remove files (e.g. docs, tests, etc.) from installed bower components
+
+## Getting Started
+This plugin requires Grunt `~0.4.1`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-bower-clean --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-bower-clean');
+```
+
+## Usage
+
+In your `bower.json` file configure `dependenciesIgnore` like so:
+
+```js
+{
+  "name": "my-app",
+  "version": "1.0.0",
+  "dependencies": {
+    "backbone": "0.9.2",
+    "when": "~2.1.1"
+  },
+  "dependenciesIgnore": {
+    "backbone": ["**/!(backbone.js)"],
+    "when": ["docs", "test", "*.!(js)"]
+  }
+}
+```
+
+Then after
+
+```js
+bower install
+```
+
+running
+
+```js
+grunt bower_clean
+```
+
+will remove all files from `bower_components/backbone` except for `backbone.js` and `.bower.json` and from `bower_components/when`, `docs` and `test` directories will be removed as well as all non JS files (excep for `.bower.json`). This works exactly like bower's own ignore option (see https://github.com/bower/bower#defining-a-package for more info).
---
+++ tasks/bower_clean.js
@@ -0,0 +1,59 @@
+/*
+ * grunt-bower-clean
+ * https://github.com/KidkArolis/grunt-bower-clean
+ *
+ * Copyright (c) 2013 Karolis Narkevicius
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var _ = require('lodash');
+var path = require('path');
+var Q = require('q');
+var bower = require('bower');
+var basePath = process.cwd();
+// var removeIgnores = require('bower/lib/util/removeIgnores');
+var removeIgnores = require('../lib/removeIgnores');
+
+
+module.exports = function(grunt) {
+
+  grunt.registerTask('bower_clean', 'Your task description goes here.', function () {
+    var done = this.async();
+
+    var dryRun = !!grunt.option('dry-run');
+
+    var cfg = require(path.join(basePath, 'bower.json')).dependenciesIgnore;
+
+    // no configuration is provided for the ignore
+    if (!cfg || _.isEmpty(cfg)) {
+      return done();
+    }
+
+
+    bower.commands
+      .list({paths: true})
+      .on('error', function (err) {
+        grunt.log.error(err.message);
+      })
+      .on('end', function (data) {
+        var removals = [];
+        _.each(data, function(dir, component) {
+          // check if we have ignore configuration for this component
+          var ignore = cfg[component];
+          if (ignore) {
+            removals.push(removeIgnores(dir, ignore, {
+              dryRun: dryRun
+            }));
+          }
+        });
+        Q.all(removals).then(function (files) {
+          done();
+        }, function (err) {
+          grunt.log.error(err.message);
+        });
+      });
+  });
+
+};
